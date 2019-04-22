0a9d6979b4d243fceae1993bfb43d6d2903e163b
mkx <1129373068@qq.com>
2019-04-22 13:32:58 +0800

    删除多余的store

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import studentModule from './studentModule.js'
 Vue.use(Vuex);
 export default new Vuex.Store({
   modules:{
-    studentModule
   }
 });
