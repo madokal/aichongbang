ea9d309664e083b923d2f5799799daed42d861a7
mkx <1129373068@qq.com>
2019-04-24 14:11:59 +0800

    整合store.index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,35 +1,22 @@
 import Vue from "vue";
 import Vuex from "vuex";
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 import productModule from './productModule.js'
 import newModule from './newModule.js'
 import studentModule from './studentModule.js'
 import shops from './shops.js'
-
+import serviceModule from './serviceModule.js'
+import serviceTypeModule from './serviceTypeModule.js'
 import orderModule from "./orderModule"
->>>>>>> 52935476ca36312b4d45ecc9cc75fad28bf40fbb
 Vue.use(Vuex);
 export default new Vuex.Store({
   strict:true,
   modules:{
-<<<<<<< HEAD
-=======
-import serviceModule from './serviceModule.js'
-import serviceTypeModule from './serviceTypeModule.js'
-Vue.use(Vuex);
-export default new Vuex.Store({
-  modules:{
     serviceModule,
-    serviceTypeModule
->>>>>>> mkx
-=======
+    serviceTypeModule,
     productModule,
     newModule,
     studentModule,
     shops,
     orderModule
->>>>>>> 52935476ca36312b4d45ecc9cc75fad28bf40fbb
   }
 });
