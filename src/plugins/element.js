b258d209d24dc1cc91cf4c9d2e114b554dc07644
LY <1126297079@qq.com>
2019-04-25 02:15:46 +0800

    增加定位

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-
+import BaiduMap from 'vue-baidu-map'
+Vue.use(BaiduMap, {
+    // ak 是在百度地图开发者平台申请的密钥 详见 http://lbsyun.baidu.com/apiconsole/key */
+    ak: 'QyDzrMFSIuK5juxzsoQ2CezSVDbpprHg'
+  })
 Vue.use(Element)
